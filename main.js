51420c8d6571730b215bfa913d96fdf7e4b10920
ikikika <iki.jito@gmail.com>
2019-05-27 15:46:55 +0800

    drag and drop geojson and render map

--- main.js
+++ main.js
@@ -5,20 +5,26 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 import sync from 'ol-hashed';
+import DragAndDrop from 'ol/interaction/DragAndDrop';
+
+const source = new VectorSource();
 
 const map = new Map({
   target: 'map-container',
-  layers: [
-    new VectorLayer({
-      source: new VectorSource({
-        format: new GeoJSON(),
-        url: './data/countries.json'
-      })
-    })
-  ],
   view: new View({
     center: [0, 0],
     zoom: 2
   })
 });
+const layer = new VectorLayer({
+  source: source
+});
+map.addLayer(layer);
+map.addInteraction(
+  new DragAndDrop({
+    source: source,
+    formatConstructors: [GeoJSON]
+  })
+);
+
 sync(map);
