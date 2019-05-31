3e21009d92bdb2bdbdc94aba9d7ce6b95a93ae16
ikikika <iki.jito@gmail.com>
2019-05-31 23:30:21 +0800

    zoomable chart

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ import Overlay from 'ol/Overlay';
 import {Icon, Style} from 'ol/style';
 
 import Chart from 'chart.js';
+import 'chartjs-plugin-zoom';
 
 const sourcePowerStations = new VectorSource();
 const renderPowerStations = new XMLHttpRequest();
@@ -159,7 +160,7 @@ map.on('singleclick', function(evt) {
 const ctx = document.getElementById('myChart');
 // eslint-disable-next-line no-unused-vars
 const myChart = new Chart(ctx, {
-  type: 'bar',
+  type: 'line',
   data: {
     labels: ['200', '400', '600', '800', '1000', '1200'],
     datasets: [
@@ -173,23 +174,7 @@ const myChart = new Chart(ctx, {
           Math.random() * 1000,
           Math.random() * 1000
         ],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
+        borderColor: 'rgb(255, 99, 132)'
       }
     ]
   },
@@ -203,6 +188,25 @@ const myChart = new Chart(ctx, {
           }
         }
       ]
+    },
+    // Container for pan options
+    pan: {
+      // Boolean to enable panning
+      enabled: true,
+
+      // Panning directions. Remove the appropriate direction to disable
+      // Eg. 'y' would only allow panning in the y direction
+      mode: 'xy'
+    },
+
+    // Container for zoom options
+    zoom: {
+      // Boolean to enable zooming
+      enabled: true,
+
+      // Zooming directions. Remove the appropriate direction to disable
+      // Eg. 'y' would only allow zooming in the y direction
+      mode: 'xy'
     }
   }
 });
